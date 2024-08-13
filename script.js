36a2aea536fefa7c8fbb4768e7f33ebaea4843cc
vinayak3210 <kumrawatvinayak@gmail.com>
2024-08-13 16:50:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function readmore(){
+    window.location.href = "about.html";
+}
+
+seeAll=()=>{
+    window.location.href = "services.html"
+}
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("open");
+}
