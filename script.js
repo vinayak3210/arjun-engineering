28c2b986fa32aef69fd82c14e1153ec36f1adf97
vinayak3210 <kumrawatvinayak@gmail.com>
2024-08-21 16:04:14 +0530

    vinayak

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function toggleMenu() {
     var navLinks = document.getElementById("nav-links");
     navLinks.classList.toggle("open");
 }
+
+
+const button = document.getElementById('See');
+const hiddenSection = document.getElementById('hidden');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    // Change the display property to 'block' (or 'flex', 'grid', etc.)
+    hiddenSection.style.display = 'block';
+    hiddenSection.style.justifyContent = 'space-evenly';
+    hiddenSection.style.flexWrap = '';
+
+    button.style.display = 'none';
+});
